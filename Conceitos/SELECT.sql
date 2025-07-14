02b44d89be2f43a1ee144b72871d8abee062239e
Guilherme Fogolin <guilhermefogolin02@gmail.com>
2025-07-14 10:20:01 -0300

    Trabalhando com SELECT

---
+++ Conceitos/SELECT.sql
@@ -0,0 +1,7 @@
+-- SELECT
+
+SELECT email FROM sales.customers; -- Uma coluna
+
+SELECT first_name, last_name, email FROM sales.customers; -- Duas colunas
+
+SELECT * FROM sales.customers; -- Todas as colunas
