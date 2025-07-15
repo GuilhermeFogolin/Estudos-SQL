0395a7391ca52e8cea53262803d3135bd91e666a
Guilherme Fogolin <guilhermefogolin02@gmail.com>
2025-07-15 17:04:24 -0300

    Consultando receita, leads, conversão e ticket médio mês a mês

---
+++ Conceitos/PROJETO01.sql
@@ -0,0 +1,37 @@
+-- Projeto 01: queries
+
+-- Query 01: Receita, leads, conversão e ticket médio mês a mês
+
+WITH leads AS
+	(SELECT
+		date_trunc('month', visit_page_date)::date  AS visit_page_month,
+		COUNT(*) AS visit_page_count
+	FROM sales.funnel
+	GROUP BY visit_page_month
+	ORDER BY visit_page_month
+	),
+
+payments AS 
+	(SELECT
+		date_trunc('month', fun.paid_date)::date AS paid_month,
+		COUNT(fun.paid_date) AS paid_count,
+		SUM(pro.price * (1 + fun.discount)) AS receita
+	FROM sales.funnel AS fun
+	LEFT JOIN sales.products AS pro
+	ON fun.product_id = pro.product_id
+	WHERE fun.paid_date IS NOT NULL
+	GROUP BY paid_month
+	ORDER BY paid_month
+	)
+
+
+SELECT
+	l.visit_page_month AS "Mês",
+	l.visit_page_count AS "Leads (#)",
+	p.paid_count AS "Vendas (#)",
+	(p.receita / 1000) AS "Receita (k, R$)", -- Colocando em milhares
+	(p.paid_count::float / l.visit_page_count::float) AS "Conversão (%)",
+	(p.receita / p.paid_count / 1000) AS "Ticket médio (k, R$)"
+FROM leads l
+LEFT JOIN payments p
+ON l.visit_page_month = p.paid_month;
