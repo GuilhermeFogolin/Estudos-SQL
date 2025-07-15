8578a0b7ac9b8a899f2bf11387808354c1a82787
Guilherme Fogolin <guilhermefogolin02@gmail.com>
2025-07-15 17:32:47 -0300

    Consultando dias da semana e total de visitas

--- Conceitos/PROJETO01.sql
+++ Conceitos/PROJETO01.sql
@@ -75,3 +75,23 @@ WHERE paid_date BETWEEN '2021-08-01' AND '2021-08-31'
 GROUP BY "Lojas"
 ORDER BY "Vendas" DESC
 LIMIT 5;
+
+-- Query 05: Dias da semana e visitas
+
+SELECT
+	EXTRACT('dow' FROM visit_page_date) AS dia_semana,
+	CASE 
+		WHEN EXTRACT('dow' FROM visit_page_date) = 0 THEN 'Domingo'
+		WHEN EXTRACT('dow' FROM visit_page_date) = 1 THEN 'Segunda'
+		WHEN EXTRACT('dow' FROM visit_page_date) = 2 THEN 'Terça'
+		WHEN EXTRACT('dow' FROM visit_page_date) = 3 THEN 'Quarta'
+		WHEN EXTRACT('dow' FROM visit_page_date) = 4 THEN 'Quinta'
+		WHEN EXTRACT('dow' FROM visit_page_date) = 5 THEN 'Sexta'
+		WHEN EXTRACT('dow' FROM visit_page_date) = 6 THEN 'Sábado'
+		ELSE NULL 
+		END AS "Dia da semana",
+	COUNT(*) AS "Visitas (#)"
+FROM sales.funnel 
+WHERE visit_page_date BETWEEN '2021-08-01' AND '2021-08-31'
+GROUP BY dia_semana
+ORDER BY dia_semana;
