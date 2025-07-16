33a2892cb6c416cc59fde7b5257776cc97c3b375
Guilherme Fogolin <guilhermefogolin02@gmail.com>
2025-07-16 10:35:27 -0300

    Consultando faixa salarial, leads e ordenação

--- Conceitos/PROJETO02.sql
+++ Conceitos/PROJETO02.sql
@@ -46,4 +46,24 @@ FROM sales.customers
 GROUP BY "Faixa etária"
 ORDER BY "Faixa etária" DESC
 
-		
+-- Query 04: Faixa salarial, leads e ordem
+
+SELECT
+	CASE
+		WHEN income < 5000 THEN '0-5000'
+		WHEN income < 10000 THEN '5000-10000'
+		WHEN income < 15000 THEN '10000-15000'
+		WHEN income < 20000 THEN '15000-20000'
+		ELSE '20000+'
+		END AS "Faixa salarial",
+	COUNT(*)::float / (SELECT COUNT(*) FROM sales.customers) AS "Leads (%)",
+		CASE
+		WHEN income < 5000 THEN 1
+		WHEN income < 10000 THEN 2
+		WHEN income < 15000 THEN 3
+		WHEN income < 20000 THEN 4
+		ELSE 5
+		END AS "Ordem"
+FROM sales.customers
+GROUP BY "Faixa salarial", "Ordem"
+ORDER BY "Ordem" DESC
