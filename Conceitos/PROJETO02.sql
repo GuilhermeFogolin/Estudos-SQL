b5509156d5f5aee5d7174ceaa879e8c2549e6a8f
Guilherme Fogolin <guilhermefogolin02@gmail.com>
2025-07-16 10:20:18 -0300

    Consultado status profissional e leads

--- Conceitos/PROJETO02.sql
+++ Conceitos/PROJETO02.sql
@@ -12,3 +12,21 @@ FROM sales.customers c
 LEFT JOIN temp_tables.ibge_genders i
 ON LOWER(c.first_name) = LOWER(i.first_name)
 GROUP BY i.gender;
+
+-- Query 02: Status profissional e leads
+
+SELECT
+	CASE
+		WHEN c.professional_status = 'freelancer' THEN 'freelancer'
+		WHEN c.professional_status = 'retired' THEN 'aposentado(a)'
+		WHEN c.professional_status = 'clt' THEN 'clt'
+		WHEN c.professional_status = 'self_employed' THEN 'autônomo(a)'
+		WHEN c.professional_status = 'other' THEN 'outro'
+		WHEN c.professional_status = 'businessman' THEN 'empresário(a)'
+		WHEN c.professional_status = 'civil_servant' THEN 'funcionário(o) público(a)'
+		WHEN c.professional_status = 'student' THEN 'estudante'
+		END AS "Status profissional",
+	(COUNT(*)::float) / (SELECT COUNT(*) FROM sales.customers) AS "Leads (%)"
+FROM sales.customers c
+GROUP BY professional_status
+ORDER BY "Leads (%)";
